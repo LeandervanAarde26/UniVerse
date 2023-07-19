522c9e8e81736c646cada57c10bf5d27c32509e0
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-07-19 15:05:39 +0200

    Initial
    Initial commit! How exciting is this?

---
+++ UniVerse/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UniVerse;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
