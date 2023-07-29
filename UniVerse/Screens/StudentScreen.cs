00acba1e83af73ea12343ddff1c87ed8b72ec95e
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-07-29 16:28:25 +0200

    Forgot to add heading.

--- UniVerse/Screens/StudentScreen.cs
+++ UniVerse/Screens/StudentScreen.cs
@@ -12,6 +12,64 @@ namespace UniVerse.Screens
     {
         public StudentScreen()
         {
+
+            Style inputStyle = new(typeof(Entry))
+            {
+                Setters =
+                {
+                    new Setter { Property = InputView.BackgroundColorProperty, Value = Color.FromHex("#2b2b2b") },
+                
+                    new Setter { Property = InputView.TextColorProperty, Value = Color.FromHex("#2B2B2B") }
+                }
+            };
+
+
+            Label pageHeading = new()
+            {
+                Text = "Students",
+                FontSize = 32,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.FromHex("#2B2B2B"),
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(15, 10, 0, 10)
+            };
+
+
+            Picker studentRole = new()
+            {
+                Style = inputStyle,
+                HorizontalOptions = LayoutOptions.End,
+                VerticalOptions = LayoutOptions.Center,
+                    Margin = new Thickness(0, 10, 20, 10)
+            };
+
+            FlexLayout topContainer = new()
+            {
+                JustifyContent = FlexJustify.SpaceBetween,
+                Direction = FlexDirection.Row,
+                Children = {
+                    pageHeading,
+                    studentRole
+                }
+            };
+
+            var listOptions = new List<String>
+            {
+                "All Students",
+                "Degree Student",
+                "Certificate Student"
+            };
+
+            foreach (var option in listOptions)
+            {
+                studentRole.Items.Add(option);
+            }
+            studentRole.SelectedItem = "All Students";
+            studentRole.TextColor = Colors.White;
+            studentRole.TitleColor = Colors.White;
+
+
             FlexLayout layout = new FlexLayout
             {
                 Direction = FlexDirection.Row,
@@ -32,6 +90,7 @@ namespace UniVerse.Screens
             {
                 RowDefinitions = new RowDefinitionCollection
                 {
+                    new RowDefinition{Height = 60},
                     new RowDefinition{Height = GridLength.Star}
                 },
 
@@ -49,15 +108,21 @@ namespace UniVerse.Screens
 
             // Add the ContentView to the Grid
             grid.Children.Add(scrollView);
-            Grid.SetRow(scrollView, 0);
+            Grid.SetRow(scrollView, 1);
             Grid.SetColumn(scrollView, 1);
             Grid.SetColumnSpan(scrollView, 1);
             grid.BackgroundColor = Color.FromHex("#F6F7FB");
 
             grid.Children.Add(right);
-            Grid.SetRow(right, 0);
             Grid.SetColumn(right, 2);
             Grid.SetColumnSpan(right, 2);
+            Grid.SetRowSpan(right, 2);  
+
+            // Add the Flexlayout for heading to the Grid
+
+            grid.Children.Add(topContainer);
+            Grid.SetRow(topContainer, 0);
+            Grid.SetColumn(topContainer, 1);
 
             Content = grid;
             var numbers = new List<int> { 1, 2, 3, 4,  2, 3, 4, 5, 2, 3, 4, 5 };
