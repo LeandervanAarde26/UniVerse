5e11d3071596c933bbb230506dfd78945d2290a8
200089_Bee! <200089@virtualwindow.co.za>
2023-08-21 13:24:13 +0200

    Update PeopleModel.cs

--- UniVerse/Models/PeopleModel.cs
+++ UniVerse/Models/PeopleModel.cs
@@ -1,11 +1,34 @@
-﻿using System;
-namespace UniVerse.Models
+﻿namespace UniVerse.Models
 {
-	public class PersonModel
+	public class PeopleModel
 	{
-		public PersonModel()
-		{
-		}
-	}
+        public class People
+        {
+            public int person_id { get; set; }
+
+            public string person_system_identifier { get; set; } = string.Empty;
+
+            public string first_name { get; set; } = string.Empty;
+
+            public string last_name { get; set; } = string.Empty;
+
+            public string person_email { get; set; } = string.Empty;
+
+            public DateTime added_date { get; set; } = DateTime.Now;
+
+            public bool person_active { get; set; } = false;
+
+            public int role { get; set; } = 3;
+
+            public string person_image = string.Empty;
+
+            public int person_credits { get; set; }
+
+            public int needed_credits { get; set; }
+
+            public string person_password { get; set; }
+
+        }
+    }
 }
 
