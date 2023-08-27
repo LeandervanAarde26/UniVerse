1a5c8f2545b6fec4eba593ac54faffa7bc5a9507
200089_Bee! <200089@virtualwindow.co.za>
2023-08-27 19:54:24 +0200

    Added Model for Student Overview

---
+++ UniVerse/Models/StudentOverviewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace UniVerse.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        public string image { get; set; }
+
+        //public string person_system_identifier { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string role { get; set; }
+
+        public string person_credits { get; set; }
+
+        public string needed_credits { get; set; }
+
+        public string person_system_identifier { get; set; }
+    }
+}
