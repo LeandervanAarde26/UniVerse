3f53297ee21635b853ca90f0afb6480b995223a0
200089_Bee! <200089@virtualwindow.co.za>
2023-08-27 22:42:06 +0200

    Subject Model Added

---
+++ UniVerse/Models/SubjectModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace UniVerse.Models
+{
+    public class SubjectWithLecturerModel
+    {
+        public SubjectModel Subject { get; set; }
+        public string LecturerName { get; set; }
+    }
+
+    public class SubjectModel
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectCode { get; set; }
+        public string SubjectDescription { get; set; }
+        public int SubjectCost { get; set; }
+        public string SubjectColor { get; set; }
+        public int LecturerId { get; set; }
+        public int SubjectCredits { get; set; }
+        public int SubjectClassRuntime { get; set; }
+        public int SubjectClassAmount { get; set; }
+        public string SubjectImage { get; set; }
+    }
+
+}
+
