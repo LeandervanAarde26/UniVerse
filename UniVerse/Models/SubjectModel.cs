1be9589b36bfb3c60e2ef8b3fad9667029c822ca
200089_Bee! <200089@virtualwindow.co.za>
2023-09-03 18:03:08 +0200

    SUBJECTS ADDDDDIIIINGGGG
    !!!!!!!!

--- UniVerse/Models/SubjectModel.cs
+++ UniVerse/Models/SubjectModel.cs
@@ -10,16 +10,18 @@ namespace UniVerse.Models
     public class SubjectModel
     {
         public int subject_id { get; set; }
-        public string subject_name { get; set; }
-        public string subject_code { get; set; }
-        public string subject_description { get; set; }
-        public int subject_cost { get; set; }
-        public string subject_color { get; set; }
+        public string subject_name { get; set; } = String.Empty;
+        public string subject_code { get; set; } = String.Empty;
+        public string subject_description { get; set; } = String.Empty;
+        public int subject_cost { get; set; } = 0;
+        public string subject_color { get; set; } = String.Empty;
         public int lecturer_id { get; set; }
-        public int subject_credits { get; set; }
-        public int subject_class_runtiem { get; set; }
-        public int subject_class_amount { get; set; }
-        public string subjectImage { get; set; }
+        public int subject_credits { get; set; } = 0;
+        public int subject_class_runtiem { get; set; } = 0;
+        public int subject_class_amount { get; set; } = 0;
+        public string subjectImage { get; set; } = String.Empty;
+        public bool is_active { get; set; }
+        public DateTime course_start { get; set; } = DateTime.Now;
     }
 }
 
--- UniVerse/Screens/SubjectsScreen.xaml.cs
+++ UniVerse/Screens/SubjectsScreen.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using UniVerse.Models;
 using UniVerse.ViewModels;
 
@@ -19,6 +18,132 @@ public partial class SubjectsScreen : ContentPage
         {
             _pickerItems = value;
             OnPropertyChanged(nameof(PickerItems));
+        }
+    }
+
+    private string _subjectName;
+    public string SubjectName
+    {
+        get { return _subjectName; }
+        set
+        {
+            if (_subjectName != value)
+            {
+                _subjectName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private string _subjectCode;
+    public string SubjectCode
+    {
+        get { return _subjectCode; }
+        set
+        {
+            if (_subjectCode != value)
+            {
+                _subjectCode = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private int _subjectCost;
+    public int SubjectCost
+    {
+        get { return _subjectCost; }
+        set
+        {
+            if (_subjectCost != value)
+            {
+                _subjectCost = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private int _subjectCredits;
+    public int SubjectCredits
+    {
+        get { return _subjectCredits; }
+        set
+        {
+            if (_subjectCredits != value)
+            {
+                _subjectCredits = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private string _subjectColor;
+    public string SubjectColor
+    {
+        get { return _subjectColor; }
+        set
+        {
+            if (_subjectColor != value)
+            {
+                _subjectColor = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private int _subjectRuntime;
+    public int SubjectRuntime
+    {
+        get { return _subjectRuntime; }
+        set
+        {
+            if (_subjectRuntime != value)
+            {
+                _subjectRuntime = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private string _subjectDescription;
+    public string SubjectDescription
+    {
+        get { return _subjectDescription; }
+        set
+        {
+            if (_subjectDescription != value)
+            {
+                _subjectDescription = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private int _subjectClassAmount;
+    public int SubjectClassAmount
+    {
+        get { return _subjectClassAmount; }
+        set
+        {
+            if (_subjectClassAmount != value)
+            {
+                _subjectClassAmount = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private DateTime _subjectStartDate;
+    public DateTime SubjectStartDate
+    {
+        get { return _subjectStartDate; }
+        set
+        {
+            if (_subjectStartDate != value)
+            {
+                _subjectStartDate = value;
+                OnPropertyChanged();
+            }
         }
     }
 
@@ -55,5 +180,56 @@ public partial class SubjectsScreen : ContentPage
             };
             subjectStackLayout.Children.Add(subjectCard);
         }
+    }
+
+    private async void addSubject(object sender, EventArgs e)
+    {
+        string subjectName = SubjectName;
+        string subjectCode = SubjectCode;
+        int subjectCost = SubjectCost;
+        int subjectCredits = SubjectCredits;
+        string subjectColor = SubjectColor;
+        int subjectRuntime = SubjectRuntime;
+        string subjectDescription = SubjectDescription;
+        int subjectClassAmount = SubjectClassAmount;
+        int selectedLecturerId = 0;
+        DateTime subjectStartDate = SubjectStartDate;
+
+
+        if (picker.SelectedItem != null)
+        {
+            var selectedLecturer = (LecturerPickerItem)picker.SelectedItem;
+            selectedLecturerId = selectedLecturer.Id;
+        }
+
+
+
+        var newSubject = new SubjectModel
+        {
+            subject_name = subjectName,
+            subject_code = subjectCode,
+            subject_description = subjectDescription,
+            subject_cost = subjectCost,
+            subject_color = subjectColor,
+            lecturer_id = selectedLecturerId,
+            subject_credits = subjectCredits,
+            subject_class_runtiem = subjectRuntime,
+            subject_class_amount = subjectClassAmount,
+            course_start = subjectStartDate
+        };
+
+        await viewModel.SaveSubject(newSubject);
+
+        SubjectName = "";
+        SubjectCode = "";
+        SubjectCost = 0;
+        SubjectCredits = 0;
+        SubjectColor = "";
+        SubjectRuntime = 0;
+        SubjectDescription = "";
+        SubjectClassAmount = 0;
+        SubjectStartDate = DateTime.Now;
+
+        await viewModel.GetAllSubjects();
     }
 }
