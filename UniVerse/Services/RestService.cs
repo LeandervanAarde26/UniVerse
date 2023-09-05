4a77542fce2578d80fb0919b8dc4fd798c1f0c2d
200089_Bee! <200089@virtualwindow.co.za>
2023-09-05 10:35:02 +0200

    Added delete req in service

--- UniVerse/Services/RestService.cs
+++ UniVerse/Services/RestService.cs
@@ -38,6 +38,7 @@ namespace UniVerse.Services
             };
         }
 
+        // Read staff memebers
         public async Task<List<Person>> RefreshDataAsync()
         {
             People = new List<Person>();
@@ -60,6 +61,7 @@ namespace UniVerse.Services
             return People;
         }
 
+        // Get staff members by id
         public async Task<LecturerWithCourses> GetLecturerByIdAsync(int id)
         {
             LecturerWithCourses Lect = new();
@@ -104,6 +106,7 @@ namespace UniVerse.Services
             return Staff;
         }
 
+        // get students
         public async Task<List<Student>> GetStudentsAsync()
         {
             Students = new List<Student>();
@@ -124,6 +127,7 @@ namespace UniVerse.Services
             return Students;
         }
 
+        // get student by id
         public async Task<SingleStudentWithCourses> GetStudentByIdAsync(int id)
         {
             SingleStudentWithCourses Student = new();
@@ -146,6 +150,23 @@ namespace UniVerse.Services
             }
 
             return Student;
+        }
+
+        //Delete people
+        public async Task DeletePersonAsync(string id)
+        {
+            Uri uri = new(string.Format(baseURL + "People/{0}", id));
+
+            try
+            {
+                HttpResponseMessage response = await _client.DeleteAsync(uri);
+                if (response.IsSuccessStatusCode)
+                    Debug.WriteLine(@"\tTodoItem successfully deleted.");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
         }
 
         // Could it be that the functions were not seperated? I think they has to be seperate. 
@@ -228,8 +249,6 @@ namespace UniVerse.Services
             }
             return StudentFee;
         }
-
-
 
         public async Task<List<AdminFees>> GetAdminFeesAsync()
         {
